3615bcefbbb3cd55b5f9b9489356a616c0feac64
asmi046 <asmi046@gmail.com>
2026-05-26 01:14:49 +0300

    +1

--- resources/views/feed/yrl.blade.php
+++ resources/views/feed/yrl.blade.php
@@ -1,4 +1,6 @@
-{{ '<?xml version="1.0" encoding="UTF-8"?>' }}
+@php
+echo '<?xml version="1.0" encoding="UTF-8"@endphp';
+?>
 <realty-feed generated-at="{{ now()->toAtomString() }}">
     @foreach ($flats as $flat)
         @php
